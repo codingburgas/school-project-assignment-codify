171f4f668ab537f4f2c88f98663e350deb18a7df
TPIvanov21 <TPIvanov21@codingburgas.bg>
2024-04-17 16:42:18 +0300

    Add hover effects to register menu buttons

--- Codify/register.cpp
+++ Codify/register.cpp
@@ -5,6 +5,12 @@ using namespace std;
 Color c6 = RAYWHITE, c7 = RAYWHITE, c8 = RAYWHITE;
 void registerMenu()
 {
+	Rectangle mousePos = { GetMouseX(), GetMouseY(), 10,10 };
+	if (CheckCollisionRecs({ 650, 475, 600, 85 }, mousePos) || CheckCollisionRecs({ 650, 600, 600, 85 }, mousePos) || CheckCollisionRecs({ 850, 775, 200, 85 }, mousePos))
+		SetMouseCursor(MOUSE_CURSOR_POINTING_HAND); else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+	if (CheckCollisionRecs({ 650, 475, 600, 85 }, mousePos)) c6 = LIGHTGRAY; else c6 = RAYWHITE;
+	if (CheckCollisionRecs({ 650, 600, 600, 85 }, mousePos)) c7 = LIGHTGRAY; else c7 = RAYWHITE;
+	if (CheckCollisionRecs({ 850, 775, 200, 85 }, mousePos)) c8 = LIGHTGRAY; else c8 = RAYWHITE;
 	BeginDrawing();
 	ClearBackground(RAYWHITE);
 	DrawRectangleRounded({ 600, 200, 700, 700 }, 0.2, 0, RAYWHITE);
