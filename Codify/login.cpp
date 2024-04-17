09e4badc36b1ba8473deac46c22f5454edec00f7
TPIvanov21 <TPIvanov21@codingburgas.bg>
2024-04-17 16:33:56 +0300

    Add hover effect to login page buttons

--- Codify/login.cpp
+++ Codify/login.cpp
@@ -5,6 +5,12 @@ using namespace std;
  Color c3 = RAYWHITE, c4 = RAYWHITE, c5 = RAYWHITE;
 void loginMenu()
 {
+	Rectangle mousePos = { GetMouseX(), GetMouseY(), 10,10 };
+	if (CheckCollisionRecs({ 650, 475, 600, 85 }, mousePos) || CheckCollisionRecs({ 650, 600, 600, 85 }, mousePos) || CheckCollisionRecs({ 850, 775, 200, 85 }, mousePos))
+		SetMouseCursor(MOUSE_CURSOR_POINTING_HAND); else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+	if (CheckCollisionRecs({ 650, 475, 600, 85 }, mousePos)) c3 = LIGHTGRAY; else c3 = RAYWHITE;
+	if (CheckCollisionRecs({ 650, 600, 600, 85 }, mousePos)) c4 = LIGHTGRAY; else c4 = RAYWHITE;
+	if (CheckCollisionRecs({ 850, 775, 200, 85 }, mousePos)) c5 = LIGHTGRAY; else c5 = RAYWHITE;
 	BeginDrawing();
 	ClearBackground(RAYWHITE);
 	DrawRectangleRounded({ 600, 200, 700, 700 }, 0.2, 0, RAYWHITE);
@@ -15,6 +21,7 @@ void loginMenu()
 	DrawRectangleRoundedLines({ 650, 600, 600, 85 }, 0.2, 0, 5, BLACK); // password input
 	DrawRectangleRounded({ 850, 775, 200, 85 }, 0.2, 0, c5);
 	DrawRectangleRoundedLines({ 850, 775, 200, 85 }, 0.2, 0, 5, BLACK); // login btn
+	DrawText("Login", 900, 800, 40, BLACK);
 	DrawText("Log Into An Existing Account", 640, 300, 40, BLACK);
 	EndDrawing();
 }
